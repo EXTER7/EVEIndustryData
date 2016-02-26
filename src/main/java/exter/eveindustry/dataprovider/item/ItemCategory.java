e2aa59bda97d04824aec4d6d44bdb279954cdfe5
EXTER <exter7@gmail.com>
2016-02-26 16:56:29 -0400

    Added more item group/category data.

--- src/main/java/exter/eveindustry/dataprovider/item/ItemCategory.java
+++ src/main/java/exter/eveindustry/dataprovider/item/ItemCategory.java
@@ -6,11 +6,15 @@ public class ItemCategory
 {
   public final int ID;
   public final String Name;
+  public final int Icon;
+  public final boolean Blueprints;
   
   public ItemCategory(TSLObject tsl)
   {
     ID = tsl.getStringAsInt("id", -1);
     Name = tsl.getString("name", null);
+    Icon = tsl.getStringAsInt("icon", -1);
+    Blueprints = tsl.getStringAsInt("blueprints", 0) == 1;
   }
 
 }
--- src/main/java/exter/eveindustry/dataprovider/item/ItemGroup.java
+++ src/main/java/exter/eveindustry/dataprovider/item/ItemGroup.java
@@ -7,11 +7,15 @@ public class ItemGroup
   public final int ID;
   public final int Category;
   public final String Name;
+  public final int Icon;
+  public final boolean Blueprints;
   
   public ItemGroup(TSLObject tsl)
   {
     ID = tsl.getStringAsInt("id", -1);
     Name = tsl.getString("name", null);
     Category = tsl.getStringAsInt("cid", -1);
+    Icon = tsl.getStringAsInt("icon", -1);
+    Blueprints = tsl.getStringAsInt("blueprints", 0) == 1;
   }
 }
